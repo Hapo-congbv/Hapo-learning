ba987415a1323472b71b1109f9737bb740298596
hapo-congbv <cong.bv@haposoft.com>
2020-07-31 15:38:42 +0700

    update banner and mess

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
+    $('.navbar-toggler').click(function () {
+        $('.icon').toggleClass('fa-times');
+        $('.icon').toggleClass('fa-bars');
+    });
+
     $('.messenger').click(function () {
         $('.toggle-class').toggle();
     });
+
     $('.close').click(function () {
         $('.toggle-class').hide();
     });
