1108f581822d2e40dab8a1cd4f5046aea23ef1e6
hapo-congbv <cong.bv@haposoft.com>
2020-08-06 11:02:22 +0700

    udpate feedback

--- js/script.js
+++ js/script.js
@@ -11,4 +11,27 @@ $(document).ready(function () {
     $('.close').click(function () {
         $('.toggle-class').hide();
     });
+
+    $('.hapo-feedback-slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        adaptiveHeight: false,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        prevArrow: $('.previous_icon'),
+        nextArrow: $('.next_icon'),
+
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 });
